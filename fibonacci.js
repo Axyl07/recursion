fc16dfe4c8e69f6a4ea5b5f1759379b8a3fbb73c
Axyl07 <aryanchauhan000777@gmail.com>
2025-01-05 19:36:52 +0530

    Finally finished recursive fibonacci on my own(previous code was partially ai generated)

--- fibonacci.js
+++ fibonacci.js
@@ -12,14 +12,32 @@ function fibs(num) {
 }
 // console.log(fibs(8));
 
-function fibsRec(n, array = [0, 1]) {
-  console.log("hi");
-  if (n <= 1) return [0];
-  if (n === 2) {
-    return array;
-  } else {
-    array.push(array[array.length - 1] + array[array.length - 2]);
-    return fibsRec(n - 1, array);
-  }
+// function fibsRec(n, array = [0, 1]) {
+//   console.log("hi");
+//   if (n <= 1) return [0];
+//   if (n === 2) {
+//     return array;
+//   } else {
+//     array.push(array[array.length - 1] + array[array.length - 2]);
+//     return fibsRec(n - 1, array);
+//   }
+// }
+
+function fibsRec2(n) {
+    console.log("This was printed recursively");
+    //base case
+    if (n < 2) {
+        let arr = [n]
+        return arr;
+    }
+    if (n == 2) {
+        let array = [0, 1];
+        return array;
+    }   
+    else {
+        let arr = fibsRec2(n - 1);
+        arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+        return arr
+    }
 }
-console.log(fibsRec(5));
+console.log(fibsRec2(8));
