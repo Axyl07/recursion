f619ee88cf7ebd894f63344aa49c22ecbf65c899
Axyl07 <aryanchauhan000777@gmail.com>
2025-01-05 17:44:34 +0530

    Recursive fibonacci sequence done

--- fibonacci.js
+++ fibonacci.js
@@ -1,14 +1,25 @@
-console.log('fib');
+console.log("fibo:");
 function fibs(num) {
-    let arr = [];
-    for (let i = 0; i < num; i++) {
-       if (i<2) {
-           arr[i] = i;
-       }
-       else {
-           arr[i] = arr[i - 1] + arr[i - 2];
-        }
+  let arr = [];
+  for (let i = 0; i < num; i++) {
+    if (i < 2) {
+      arr[i] = i;
+    } else {
+      arr[i] = arr[i - 1] + arr[i - 2];
     }
-    return arr;
+  }
+  return arr;
 }
-console.log(fibs(8));
+// console.log(fibs(8));
+
+function fibsRec(n, array = [0, 1]) {
+  console.log("hi");
+  if (n <= 1) return [0];
+  if (n === 2) {
+    return array;
+  } else {
+    array.push(array[array.length - 1] + array[array.length - 2]);
+    return fibsRec(n - 1, array);
+  }
+}
+console.log(fibsRec(5));
