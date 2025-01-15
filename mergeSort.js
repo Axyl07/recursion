31af8eb69050cb945d695da940c97ad00426b2d7
Axyl07 <aryanchauhan000777@gmail.com>
2025-01-15 15:00:48 +0530

    Implement splitting part

--- mergeSort.js
+++ mergeSort.js
@@ -1,31 +1,23 @@
 console.log("merge");
 
-// function split(array, low = 0, high = array.length - 1) {
-//   console.log("low = " + low);
-//   console.log("high = " + high);
-//   if (low < high) {
+function split(array, low = 0, high = array.length - 1) {
+  let mid = parseInt((low + high) / 2);
+  let leftarray = array.slice(low, mid + 1);
+  let rightarray = array.slice(mid + 1, high + 1);
+  return [leftarray, rightarray];
+}
+console.log(split([1, 2, 3, 4, 5, 6, 7, 8]));
+// function mergeSort(array, low = 0, high = array.length - 1) {
+//   if (low<high) {
 //     let mid = parseInt((low + high) / 2);
-//     console.log("mid = " + mid);
-//     let left = split(array, low, mid);
-//     let right = split(array, mid + 1, high);
-//     console.log("left = " + left);
-//     console.log("right = " + right);
-//     return left;
+//     let leftArray = mergeSort(array, low, mid);
+//     let rightArray = mergeSort(array, mid + 1, high);
+//     return merge(leftArray, rightArray);
 //   }
-//   return array;
+//   else {
+//     return array;
+//   }
 // }
-
-// function split(array, low = 0, high = array.length - 1) {
-
-//     let mid = parseInt((low + high) / 2);
-//     console.log("mid is " + mid);
-//     let leftArray = array.slice(low, mid + 1);
-//     let rightArray = array.slice(mid + 1, high);
-//     console.log(leftArray);
-//     console.log(rightArray);
-
-// }
-// console.log(split([1, 2, 3, 4, 5, 6]));
 
 function merge(leftArray, rightArray) {
   let m = leftArray.length;
@@ -36,7 +28,7 @@ function merge(leftArray, rightArray) {
     k = 0;
   while (i < m && j < n) {
     if (leftArray[i] < rightArray[j]) {
-      outputArray[k++] = leftArray[i++];     
+      outputArray[k++] = leftArray[i++];
     } else {
       outputArray[k++] = rightArray[j++];
     }
@@ -44,12 +36,10 @@ function merge(leftArray, rightArray) {
   while (i < m) {
     outputArray[k++] = leftArray[i++];
   }
-
   while (j < n) {
     outputArray[k++] = rightArray[j++];
   }
- 
   return outputArray;
 }
 
-console.log(merge([1,7], [2,4]));
+// console.log(merge([1, 7], [2, 4]));
