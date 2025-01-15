af92902b1124eae26c240e49f8dafb87c9a72eff
Axyl07 <aryanchauhan000777@gmail.com>
2025-01-15 17:56:14 +0530

    Trying to implement merge sort but only left part is sorted currently

--- mergeSort.js
+++ mergeSort.js
@@ -1,45 +1,45 @@
 console.log("merge");
 
-function split(array, low = 0, high = array.length - 1) {
-  let mid = parseInt((low + high) / 2);
-  let leftarray = array.slice(low, mid + 1);
-  let rightarray = array.slice(mid + 1, high + 1);
-  return [leftarray, rightarray];
-}
-console.log(split([1, 2, 3, 4, 5, 6, 7, 8]));
-// function mergeSort(array, low = 0, high = array.length - 1) {
-//   if (low<high) {
-//     let mid = parseInt((low + high) / 2);
-//     let leftArray = mergeSort(array, low, mid);
-//     let rightArray = mergeSort(array, mid + 1, high);
-//     return merge(leftArray, rightArray);
-//   }
-//   else {
-//     return array;
-//   }
-// }
-
-function merge(leftArray, rightArray) {
-  let m = leftArray.length;
-  let n = rightArray.length;
-  let outputArray = [];
-  let i = 0,
-    j = 0,
-    k = 0;
-  while (i < m && j < n) {
-    if (leftArray[i] < rightArray[j]) {
-      outputArray[k++] = leftArray[i++];
-    } else {
-      outputArray[k++] = rightArray[j++];
+  function mergeSort(array, low = 0, high = array.length - 1) {
+    if (array.length < 2) {
+      return array;
+    }
+    else {
+      let mid = parseInt((low + high) / 2);
+      let left = array.slice(low, mid + 1);
+      let right = array.slice(mid + 1, high + 1);
+      let leftSubArray = mergeSort(left, low, mid);
+      console.log(leftSubArray);
+      let rightSubArray = mergeSort(right, mid + 1, high);
+      console.log(rightSubArray);
+      // (leftSubArray, rightSubArray);
+      // mergeSort(array, low, high,outputArray);
+      return merge(leftSubArray,rightSubArray);
+      
     }
   }
-  while (i < m) {
-    outputArray[k++] = leftArray[i++];
-  }
-  while (j < n) {
-    outputArray[k++] = rightArray[j++];
-  }
-  return outputArray;
-}
 
-// console.log(merge([1, 7], [2, 4]));
+  console.log("sorted: " + mergeSort([4,3,2,1]));
+  function merge(leftArray, rightArray) {
+    let m = leftArray.length;
+    let n = rightArray.length;
+    let outputArray = [];
+    let i = 0,
+      j = 0,
+      k = 0;
+    while (i < m && j < n) {
+      if (leftArray[i] < rightArray[j]) {
+        outputArray[k++] = leftArray[i++];
+      } else {
+        outputArray[k++] = rightArray[j++];
+      }
+    }
+    while (i < m) {
+      outputArray[k++] = leftArray[i++];
+    }
+    while (j < n) {
+      outputArray[k++] = rightArray[j++];
+    }
+    return outputArray;
+  }
+
